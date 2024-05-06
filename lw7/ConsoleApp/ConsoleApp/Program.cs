8b2c9f5680235f9bbe4b17657a444c7e184544cc
KirillNaRabote <kirillznaet@mail.ru>
2024-05-07 02:29:32 +0300

    эээээ

---
+++ lw7/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExchangeRate exchangeRate = new ExchangeRate();
+
+        Console.WriteLine(exchangeRate.GetRateFromCurrency("booba"));;
+    }
+}
